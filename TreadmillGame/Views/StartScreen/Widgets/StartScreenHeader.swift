5a5197152125d8f94fe2e6617d461a56ce3d5cec
Yousef Shams <yousef.shams@akqa.com>
2026-03-02 22:24:54 +0200

    added treadmill animation assets and update StartScreenHeader to use local Lottie asset

--- TreadmillGame/Views/StartScreen/Widgets/StartScreenHeader.swift
+++ TreadmillGame/Views/StartScreen/Widgets/StartScreenHeader.swift
@@ -4,10 +4,9 @@ import Lottie
 struct StartScreenHeader: View {
     var body: some View {
         HStack(spacing: AppSpacing.lg) {
-            LottieView {
-                await LottieAnimation.loadedFrom(url: URL(string:"https://lottie.host/8935aae4-7ac7-47b9-8170-bcd24d9bcd24/DSkuv45pet.json")! )
-            }.playing()
-            .frame(maxWidth: 70, maxHeight: 70)
+            LottieView(animation: .asset("treadmill")).playing(loopMode: .loop)
+                .frame(maxWidth: 70, maxHeight: 70)
+            
             VStack(alignment: .leading, spacing: AppSpacing.sm) {
                 Text(AppText.StartScreen.title)
                     .font(AppFonts.largeTitle())
